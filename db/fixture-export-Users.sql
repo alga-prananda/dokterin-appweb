d329882d9c5280aae9809c7d2da6cd58fef00add
Alga Prananda <68110371+alga-prananda@users.noreply.github.com>
2021-12-20 01:07:16 +0700

    Delete fixture-export-Users.sql

--- db/fixture-export-Users.sql
+++
@@ -1,5 +0,0 @@
-/*
--- Query: SELECT * FROM `Users`
-*/
-
-INSERT INTO `Users` (`id`,`email`,`password`,`type`,`createdAt`,`updatedAt`,`Patient_id`) VALUES (1,'alga@test.com','alga123','patient','2021-12-08 23:13:29','2021-12-13 23:13:29',1);
